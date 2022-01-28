5e651d0ccb3f7317a0cb4355c707fe23c5cd914b
Alexander Goldberg <alexander.goldberg@bnymellon.com>
2022-01-28 15:17:41 -0500

    Added test coverage

--- eclipse-collections/src/test/java/com/fasterxml/jackson/datatype/eclipsecollections/serializedeserialize/TupleTests.java
+++ eclipse-collections/src/test/java/com/fasterxml/jackson/datatype/eclipsecollections/serializedeserialize/TupleTests.java
@@ -2,12 +2,12 @@ package com.fasterxml.jackson.datatype.eclipsecollections.serializedeserialize;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.eclipsecollections.ModuleTestBase;
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.api.tuple.Pair;
 import org.eclipse.collections.api.tuple.Twin;
 import org.eclipse.collections.impl.tuple.Tuples;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class TupleTests
@@ -15,18 +15,19 @@ public class TupleTests
     @Test
     public void twin() throws JsonProcessingException {
         Twin<String> twin = Tuples.twin("One", "Two");
-        ObjectMapper objectMapper = this.mapperWithModule();
-        String s = objectMapper.writeValueAsString(twin);
-        Twin<String> deserialized = objectMapper.readValue(s, new TypeReference<Twin<String>>() {});
-        Assert.assertEquals(twin, deserialized);
+        this.verifySerializeDeserialize(twin, new TypeReference<Twin<String>>() {});
     }
 
     @Test
     public void pair() throws JsonProcessingException {
         Pair<String, Integer> pair = Tuples.pair("A", 25);
-        ObjectMapper objectMapper = this.mapperWithModule();
-        String s = objectMapper.writeValueAsString(pair);
-        Pair<String, Integer> deserialized = objectMapper.readValue(s, new TypeReference<Pair<String, Integer>>() {});
-        Assert.assertEquals(deserialized, deserialized);
+        this.verifySerializeDeserialize(pair, new TypeReference<Pair<String, Integer>>() {});
+    }
+
+    @Test
+    public void listOfPairs() throws JsonProcessingException {
+        MutableList<Pair<String, Integer>> pairs =
+                Lists.mutable.of(Tuples.pair("A", 25), Tuples.pair("B", 250));
+        this.verifySerializeDeserialize(pairs, new TypeReference<MutableList<Pair<String, Integer>>>() {});
     }
 }
